7733e70c6aa864f17e6c0dff02b3a30307b0a354
Nathan Lyle Black <lyleblack@gmail.com>
2015-07-06 22:38:08 -0700

    form tests

---
+++ test/helpers.js
@@ -0,0 +1,88 @@
+var mongoose = require('mongoose'),
+    fs       = require('fs'),
+    path     = require('path'),
+    assign   = require('object-assign'),
+    async    = require('async'),
+    Offering = require('../app/db.js').Offering,
+    Roastery = require('../app/db.js').Roastery;
+
+function seedDatabase(finished) {
+  async.waterfall([
+
+    // Get seed data.
+    function(callback) {
+      var filename = path.join(__dirname, 'seeds.json');
+      fs.readFile(filename, function(err, data) {
+        if (err) throw err;
+        callback(null, JSON.parse(data));
+      });
+    },
+
+    // Create two different roasteries.
+    function(data, callback) {
+      var halfwit   = data.Roasteries[0],
+          gaslight  = data.Roasteries[1],
+          offerings = data.Offerings;
+
+      Roastery.create(halfwit, gaslight, function(err, Halfwit, Gaslight) {
+        if (err) throw err;
+
+        var roasterInfo = {
+          Halfwit:  { _id: Halfwit._id, name: Halfwit.name },
+          Gaslight: { _id: Gaslight._id, name: Gaslight.name }
+        };
+
+        callback(null, roasterInfo, offerings);
+      });
+    },
+
+    // Create offerings.
+    function(roasterInfo, offerings, callback) {
+
+      var Halfwit  = roasterInfo.Halfwit,
+          Gaslight = roasterInfo.Gaslight;
+
+      var decaf   = assign({}, offerings.decaf, { roastery: Halfwit }),
+          organic = assign({}, offerings.organic, { roastery: Halfwit }),
+          direct  = assign({}, offerings.direct, { roastery: Halfwit }),
+          blend   = assign({}, offerings.blend, { roastery: Gaslight }),
+          honey   = assign({}, offerings.honey, { roastery: Gaslight });
+
+      var allOfferings = [decaf, organic, direct, blend, honey];
+
+      Offering.create(allOfferings, function(err, Offerings) {
+        if (err) throw err;
+        finished();
+      });
+    }
+  ]);
+}
+
+function clearDatabase(finished) {
+  async.parallel([
+
+    function(callback) {
+      Offering.remove({}, function(err) {
+        if (err) callback(err);
+        callback(null);
+      });
+    },
+
+    function(callback) {
+      Roastery.remove({}, function(err) {
+        if (err) callback(err);
+        callback(null);
+      });
+    }
+  ],
+
+  function(err, results) {
+    if (err) throw err;
+    finished();
+  });
+}
+
+module.exports = {
+  seedDatabase: seedDatabase,
+  clearDatabase: clearDatabase
+};
---
+++ test/test-form.js
@@ -0,0 +1,285 @@
+var mongoose       = require('mongoose'),
+    React          = require('react'),
+    ReactAddons    = require('react/addons'),
+    ReactTestUtils = React.addons.TestUtils,
+    Simulate       = ReactTestUtils.Simulate,
+    should         = require('should'),
+    app            = require('../server'),
+    assign         = require('object-assign'),
+    request        = require('supertest'),
+    helpers        = require('./helpers.js'),
+    offeringCount;
+
+describe('Form', function() {
+
+  var defaultInputs = {
+    blend:   false,
+    decaf:   false,
+    direct:  false,
+    organic: false,
+    origin:  'Any',
+    process: 'Any',
+    roaster: 'Any',
+    search:  ''
+  };
+
+  before(function(done) {
+    helpers.seedDatabase(done);
+  });
+
+  describe('checkboxes', function() {
+    describe('decaf', function() {
+
+      it('should return 1 coffee when checked', function(done) {
+        var input = assign({}, defaultInputs, { decaf: true });
+
+        request(app)
+          .post('/offerings/find')
+          .send(input)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(1);
+            done();
+          });
+      });
+
+      it('should return 5 coffees when not checked', function(done) {
+        request(app)
+          .post('/offerings/find')
+          .send(defaultInputs)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(5);
+            done();
+          });
+      });
+    });
+
+    describe('organic', function() {
+
+      it('should return 1 coffee when checked', function(done) {
+        var input = assign({}, defaultInputs, { organic: true });
+
+        request(app)
+          .post('/offerings/find')
+          .send(input)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(1);
+            done();
+          });
+      });
+
+      it('should return 5 coffees when not checked', function(done) {
+        request(app)
+          .post('/offerings/find')
+          .send(defaultInputs)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(5);
+            done();
+          });
+      });
+    });
+
+    describe('blend', function() {
+
+      it('should return 1 coffee when checked', function(done) {
+        var input = assign({}, defaultInputs, { blend: true });
+
+        request(app)
+          .post('/offerings/find')
+          .send(input)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(1);
+            done();
+          });
+      });
+
+      it('should return 5 coffees when not checked', function(done) {
+        request(app)
+          .post('/offerings/find')
+          .send(defaultInputs)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(5);
+            done();
+          });
+      });
+    });
+
+    describe('direct trade', function() {
+
+      it('should return 1 coffee when checked', function(done) {
+        var input = assign({}, defaultInputs, { direct: true });
+
+        request(app)
+          .post('/offerings/find')
+          .send(input)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(1);
+            done();
+          });
+      });
+
+      it('should return 5 coffees when not checked', function(done) {
+        request(app)
+          .post('/offerings/find')
+          .send(defaultInputs)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(5);
+            done();
+          });
+      });
+    });
+  });
+
+  describe('select inputs', function() {
+    describe('origin', function() {
+
+      it('should filters coffees based on origin', function(done) {
+        var input = assign({}, defaultInputs, { origin: 'Kenya' });
+
+        request(app)
+          .post('/offerings/find')
+          .send(input)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(2);
+            done();
+          });
+      });
+
+      it("shouldn't filter when value is 'any'", function(done) {
+        request(app)
+          .post('/offerings/find')
+          .send(defaultInputs)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(5);
+            done();
+          });
+      });
+    });
+
+    describe('roaster', function() {
+
+      it('should filters coffees based on roastery', function(done) {
+        var input = assign({}, defaultInputs, { roaster: 'Halfwit' });
+
+        request(app)
+          .post('/offerings/find')
+          .send(input)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(3);
+            done();
+          });
+      });
+
+      it("shouldn't filter when value is 'any'", function(done) {
+        request(app)
+          .post('/offerings/find')
+          .send(defaultInputs)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(5);
+            done();
+          });
+      });
+    });
+
+    describe('process', function() {
+
+      it('should filters coffees based on process', function(done) {
+        var input = assign({}, defaultInputs, { process: 'Natural' });
+
+        request(app)
+          .post('/offerings/find')
+          .send(input)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(2);
+            done();
+          });
+      });
+
+      it("shouldn't filter when value is 'any'", function(done) {
+        request(app)
+          .post('/offerings/find')
+          .send(defaultInputs)
+          .expect(200)
+          .end(function(err, res) {
+            var body = JSON.parse(res.body);
+            if (err) return done(err);
+            body.length.should.equal(5);
+            done();
+          });
+      });
+    });
+  });
+
+  describe('search box', function() {
+    it('should return nothing when no descriptions match', function(done) {
+      var input = assign({}, defaultInputs, { search: 'gasoline' });
+      request(app)
+        .post('/offerings/find')
+        .send(input)
+        .expect(200)
+        .end(function(err, res) {
+          var body = JSON.parse(res.body);
+          if (err) return done(err);
+          body.length.should.equal(0);
+          done();
+        });
+    });
+
+    it('should return match ', function(done) {
+      var input = assign({}, defaultInputs, { search: 'gin' });
+      request(app)
+        .post('/offerings/find')
+        .send(input)
+        .expect(200)
+        .end(function(err, res) {
+          var body = JSON.parse(res.body);
+          if (err) return done(err);
+          body.length.should.equal(1);
+          done();
+        });
+    });
+  });
+
+  after(function(done) {
+    helpers.clearDatabase(done);
+  });
+
+});

5443422eb88de0c0b4b9ac30ca340c1cedbfe6f9
Nathan Lyle Black <lyleblack@gmail.com>
2015-08-12 20:56:35 -0700

    switch to mongolab url

--- app/db.js
+++ app/db.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
-var mongoURI = process.env.NODE_ENV === 'test' ?
-  'mongodb://localhost/testCoffee':
-  'mongodb://localhost/getCoffee';
+var mongoURL = process.env.NODE_ENV === 'test' ?
+  process.env.MONGOLAB_TEST_URL :
+  process.env.MONGOLAB_COFFEE_URL;
 
-mongoose.connect(mongoURI);
+mongoose.connect(process.env.MONGOLAB_COFFEE_URL);
 
 // Load models.
 require(__dirname + '/models/Offering.js');
